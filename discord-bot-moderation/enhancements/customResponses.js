43c0b9b7919ed2ab082059a0be442e60c8a4d2fe
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 15:04:21 +0530

    Added: `discord-bot-moderation/enhancements/customResponses.js`

---
+++ discord-bot-moderation/enhancements/customResponses.js
@@ -0,0 +1,15 @@
+const customResponses = {
+  welcomeMessage: "Welcome to the server!",
+  goodbyeMessage: "Goodbye, see you next time!",
+  customCommands: {
+    "!help": "Display a list of available commands.",
+    "!rules": "Show the server rules.",
+    "!info": "Provide information about the server."
+  },
+  automatedResponses: {
+    swearWordWarning: "Please refrain from using inappropriate language.",
+    userBanned: "You have been banned from the server."
+  }
+};
+
+module.exports = customResponses;
